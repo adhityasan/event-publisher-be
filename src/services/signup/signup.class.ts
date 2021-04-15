9b164c770c64d29a01628e9ff91a336c6b9eabed
adhityasan <adhityad3v@gmail.com>
2021-04-15 13:19:16 +0700

    feat: 🎸 signup service

---
+++ src/services/signup/signup.class.ts
@@ -0,0 +1,37 @@
+import { ServiceMethods } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+import { generateVerifyToken } from '../../utils/helpers';
+
+interface Data {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface ServiceOptions {}
+
+export class Signup implements Partial<ServiceMethods<Data>> {
+  app: Application;
+  options: ServiceOptions;
+
+  constructor(options: ServiceOptions = {}, app: Application) {
+    this.options = options;
+    this.app = app;
+  }
+
+  async create(data: Data): Promise<Data> {
+    // NOTE: create new user
+    const newUser = await this.app.service('users').create(data);
+    const tokenPayload = {
+      _id: newUser._id,
+      email: data.email,
+      name: data.name
+    };
+    const tokenSecret: string = this.app.get('authentication').secret;
+    const verifyToken = generateVerifyToken(tokenPayload, tokenSecret);
+
+    this.app.service('users').patch(newUser._id, { verifyToken });
+
+    return data;
+  }
+}
